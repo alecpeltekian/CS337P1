debd07d4338ef6b944f20b0f40deaf6a98e046dc
Alec Peltekian <alecpeltekian@dhcp-10-105-199-244.wireless.northwestern.private>
2023-01-19 17:37:46 -0600

    Tweet Text Seperation

--- preprocess.py
+++ preprocess.py
@@ -1,9 +1,9 @@
 import json
+import re
 
 f = open('gg2013.json')
 data = json.load(f)
 
-import re
 def textSplit(dataset):
     for index in range(len(dataset)):
         for key in dataset[index]:
