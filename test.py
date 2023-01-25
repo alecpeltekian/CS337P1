e38ad62f740c5b57fd4745e786d02262b34fdf3a
Siby Suriyan <sibysuriyancareer@gmail.com>
2023-01-25 15:38:33 -0600

    test(which producers winners) and terminatingList

--- test.py
+++ test.py
@@ -1,7 +1,33 @@
 import re
+import collections
+with open("terminatingList.txt") as f:
+        excludeList = f.read().splitlines()
+
+def nameToRegex(name: str, entity) -> list:
+    names = re.split(" ", name)
+    if entity == "person":
+        return f"(?i).*({'|'.join(names)}).*"
+    elif entity == "movie":
+        names = [x for x in names if x not in excludeList]
+        names = "(?i)(?=.*" + ")(?=.*".join(names) + ").*"
+        return names
+    else:
+        print(f"{entity} is Not A movie or Person!!")
+    
+# Open the input file containing the tweets
+with open('winnerTweets.txt', 'r') as input_file:
+    tweets = input_file.readlines()
 
 
-nomineeListBestPicDrama = [
+
+
+
+
+
+
+
+#outputs best motion pic-drama
+BestPicDrama = [
     "Argo",
     "Django Unchained",
     "Life of Pi",
@@ -9,15 +35,35 @@ nomineeListBestPicDrama = [
     "Zero Dark Thirty"
 ]
 
+#outputs best motion pic-musical
+BestPicMusic = [
+    "Les Miserables",
+    "The Best Exotic Marigold Hotel",
+    "Moonrise Kingdom",
+    "Salmon Fishing in the Yemen",
+    "Silver Lining Playbook"
+]
 
-# Open the input file containing the tweets
-with open('winnerTweets.txt', 'r') as input_file:
-    tweets = input_file.readlines()
+BestActressDrama = [
+    'Jessica Chastain',
+    'Marion Cotillard',
+    'Helen Mirren',
+    'Naomi Watts',
+    'Rachel Weisz'
+]
 
-dictt = {}
-for t in tweets:
-    for n in nomineeListBestPicDrama:
-        if re.search(n, t,re.IGNORECASE):
-            dictt[n] = 1 + dictt.get(n,0)
 
-print(dictt)
+def getWinner(nomineeList, entity):
+    nomineeDict = {}
+    for nom in nomineeList:
+        nomineeDict[nom] = nameToRegex(nom,entity)
+    dictt = {}
+    for t in tweets:
+        for nominee, regex in nomineeDict.items():
+            if re.search(regex,t):
+                dictt[nominee] = 1 + dictt.get(nominee,0)
+    print(max(dictt, key=dictt.get))
+
+getWinner(BestPicDrama,"movie")
+getWinner(BestPicMusic,"movie")
+getWinner(BestActressDrama,"person")
