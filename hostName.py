1d27c28736a02ca898d15619cfe231cc84fecfda
Alex Militchinski <alexmilitchinski2024@u.northwestern.edu>
2023-01-27 16:19:43 -0600

    prints host names

--- hostName.py
+++ hostName.py
@@ -1,9 +1,52 @@
-import json
+import re
+import numpy as np
+import spacy
+from spacy import displacy
+import heapq
 
-f = open('gg2013.json')
-data = json.load(f)
-print(data)
+NER = spacy.load("en_core_web_sm")
 
-def giveHost(ceremony):
-    ##
-    return ceremony
+with open('TweetText.txt', 'r') as input_file:
+    tweets = input_file.readlines()
+
+def host_names(tweets):
+    host_regex = r"(host[s]?[:]?\s)([A-Z][a-z]+(?:\s[A-Z][a-z]+)*)" 
+    host_list = parse_tweets(tweets, host_regex)
+    return host_list
+
+def parse_tweets(tweets, regexp, list=[]):
+    '''Take Tweet and seperate out id and text, search text for regexp, if match then add to dictionary'''
+    for tweet in tweets:
+        match =  re.search(regexp, tweet)
+        if(match != None):
+            extracted = match.group(0)
+            list.append(extracted)
+    return list
+
+def HostVoting(hostlist):
+    names = []
+    votes = []
+
+    for i in hostlist:
+        doc = NER(i)
+        for word in doc.ents:
+            if (word.label_ == "PERSON"):
+                if word.text in names:
+                    votes[names.index(word.text)] += 1
+                else:
+                    names.append(word.text)
+                    votes.append(1)
+    return (names, votes)
+
+x = host_names(tweets)
+
+name, vote = HostVoting(x)
+ind = [vote.index(x) for x in sorted(vote, reverse=True)[:2]]
+#print(name)
+print(name[ind[0]], name[ind[1]])
+#print(x)
+with open('hostnames.txt', 'w') as output:
+    for word in x:
+        output.write('   ')
+        output.write(word)
+        output.write('\r')
