058c9d2c4d353619c32f0452a4e2aaf2f513ba06
Alex Militchinski <alexmilitchinski2024@u.northwestern.edu>
2023-01-27 16:38:28 -0600

    improved hostName.py regex

--- hostName.py
+++ hostName.py
@@ -10,17 +10,19 @@ with open('TweetText.txt', 'r') as input_file:
     tweets = input_file.readlines()
 
 def host_names(tweets):
-    host_regex = r"(host[s]?[:]?\s)([A-Z][a-z]+(?:\s[A-Z][a-z]+)*)" 
+    host_regex = [r"(host[s]?[:]?\s)([A-Z][a-z]+(?:\s[A-Z][a-z]+)*)", 
+     r"(host[s]?[:]?\s)([A-Z][a-z]+(?:\s[A-Z][a-z]+))[\s]+(and|&)[\s]+([A-Z][a-z]+(?:\s[A-Z][a-z]+))"]
     host_list = parse_tweets(tweets, host_regex)
     return host_list
 
 def parse_tweets(tweets, regexp, list=[]):
     '''Take Tweet and seperate out id and text, search text for regexp, if match then add to dictionary'''
-    for tweet in tweets:
-        match =  re.search(regexp, tweet)
-        if(match != None):
-            extracted = match.group(0)
-            list.append(extracted)
+    for reg in regexp:
+        for tweet in tweets:
+            match =  re.search(reg, tweet)
+            if(match != None):
+                extracted = match.group(0)
+                list.append(extracted)
     return list
 
 def HostVoting(hostlist):
@@ -39,14 +41,13 @@ def HostVoting(hostlist):
     return (names, votes)
 
 x = host_names(tweets)
-
 name, vote = HostVoting(x)
 ind = [vote.index(x) for x in sorted(vote, reverse=True)[:2]]
 print(name[ind[0]], name[ind[1]])
 with open('hostnames.txt', 'w') as output:
-        output.write('   ')
-        output.write(name[ind[0]])
-        output.write('\r')
-        output.write('   ')
-        output.write(name[ind[1]])
-        output.write('\r')
+    output.write('   ')
+    output.write(name[ind[0]])
+    output.write('\r')
+    output.write('   ')
+    output.write(name[ind[1]])
+    output.write('\r')
